c6d7f4bfc0b1d993e4fb69321ca6ccf5409c279b
root <root@ubuntu.(none)>
2012-06-23 12:30:23 -0700

    Improve the readme file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '364429fc940b141636544d86fee0447d5280e369dbd061f2f3f68782325a0c24f8e6ce27a7c12de46a8505e9966b38b9775e033345bcd704c2632a5680687512'
